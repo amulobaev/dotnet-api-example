4c1146a12eb63e21ff3d8f1c33a0adff54a5fa0f
Anton Mulobaev <anton@mulobaev.ru>
2026-05-25 14:43:06 +0500

    Create project for generated client

--- src/ApiGateway/Controllers/OrdersController.cs
+++ src/ApiGateway/Controllers/OrdersController.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
-using ApiGateway.Contracts.Orders;
+using OrdersApi.Client;
 using Refit;
 using System.Net;
 
--- src/ApiGateway/Program.cs
+++ src/ApiGateway/Program.cs
@@ -1,4 +1,4 @@
-using ApiGateway.Contracts.Orders;
+using OrdersApi.Client;
 using Refit;
 
 namespace ApiGateway;
